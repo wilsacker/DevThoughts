0b20bfa8210190f112b2854a4c20e6be631afc4a
wilsacker <williamsuttona@gmail.com>
2024-09-27 16:37:40 -0700

    new post being made

--- controllers/postRoutes.js
+++ controllers/postRoutes.js
@@ -44,7 +44,8 @@ router.get('/post/:id', async (req, res) => {
 router.post('/', withAuth, async (req, res) => {
   try {
     const newPost = await Post.create({
-      ...req.body,
+      title: req.body.title,
+      content: req.body.content,
       user_id: req.session.user_id,
     });
     res.status(200).json(newPost);
---
+++ public/js/new-post.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const newPostForm = document.querySelector('#new-post-form');
+  
+    newPostForm.addEventListener('submit', async (event) => {
+      event.preventDefault();
+  
+      const title = document.querySelector('#title').value.trim();
+      const content = document.querySelector('#content').value.trim();
+  
+      if (title && content) {
+        const response = await fetch('/posts', {
+          method: 'POST',
+          body: JSON.stringify({ title, content }),
+          headers: { 'Content-Type': 'application/json' },
+        });
+  
+        if (response.ok) {
+          document.location.replace('/dashboard');
+        } else {
+          alert('Failed to create post');
+        }
+      }
+    });
+  });
