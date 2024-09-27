821da7b7fabb61e7c87cc97860ac02128f4ddc67
wilsacker <williamsuttona@gmail.com>
2024-09-27 02:20:20 -0700

    connections and routes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const { Post, User } = require('../models');
+
+router.get('/', async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [User],
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+    res.render('homepage', { posts, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
