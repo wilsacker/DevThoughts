604e357672d12fa0c0dcd63007af32122a72ecab
wilsacker <williamsuttona@gmail.com>
2024-09-27 02:32:57 -0700

    update controllers/index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
+const postRoutes = require('./postRoutes');
+const commentRoutes = require('./commentRoutes');
 const homeRoutes = require('./homeRoutes');
 
 router.use('/', homeRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
