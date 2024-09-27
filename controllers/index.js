6905bfc4fb9c4d184f174c8b16926a2212653ec3
wilsacker <williamsuttona@gmail.com>
2024-09-27 02:36:38 -0700

    userRoutes.js and update controllers/index.js

--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const postRoutes = require('./postRoutes');
 const commentRoutes = require('./commentRoutes');
 const homeRoutes = require('./homeRoutes');
+const userRoutes = require('./userRoutes');
 
 router.use('/', homeRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
